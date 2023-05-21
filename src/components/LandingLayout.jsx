b2db759ee10ebd03fe007b0a5f3ad93084206dfd
rafuj <raihanrafuj9978@gmail.com>
2023-05-21 23:20:30 +0600

    redirect to landing

--- src/components/LandingLayout.jsx
+++ src/components/LandingLayout.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import useScreen from "../hooks/useScreen";
 import Footer from "./Footer";
 import Header from "./Header";
 
 const LandingLayout = ({ children }) => {
 	const screen = useScreen();
+	const navigate = useNavigate();
+	useEffect(() => {
+		if (screen >= 576) {
+			navigate("/");
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [screen]);
 	return (
 		<>
 			<Header />
